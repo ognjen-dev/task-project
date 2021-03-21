32101aceda543dc4a9a43b7b192d65a34d4ff44d
Ognjen Vukotic <ognjendev.3@gmail.com>
2021-03-21 21:33:34 +0100

    add new font

--- src/app/components/Body.jsx
+++ src/app/components/Body.jsx
@@ -4,7 +4,7 @@ import BodyRight from "app/components/BodyRight";
 
 const Body = () => {
   return (
-    <div className="lg:px-52 md:px-32 px-1 flex flex-row justify-center mt-5 py-5">
+    <div className="font-body lg:px-52 md:px-32 px-1 flex flex-row justify-center mt-5 py-5">
       <div className="rounded-md w-full h-screen lg:p-0 mx-1 lg:w-4/6 lg:mr-4">
         <BodyLeft />
       </div>
